84aea1aad88a1ef5f0003bf0374a738bd1d547c4
RitterAntoine <116518867+RitterAntoine@users.noreply.github.com>
2024-10-29 14:57:03 +0100

    feat: training improved

--- src/config_loader.py
+++ src/config_loader.py
@@ -1,6 +1,6 @@
 import json
 
-def load_config(config_path='../config.json'):
+def load_config(config_path='config.json'):
     """Loads the configuration from a JSON file."""
     with open(config_path, 'r') as config_file:
         config = json.load(config_file)
